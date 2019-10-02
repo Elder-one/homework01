66c6af19b637d1bd7fda441c343f3bcb78b6a4bb
Elder-one <aleksan672@gmail.com>
2019-10-03 02:16:55 +0300

    Реализованы ф-ции vigenere_encrypt() и vigenere_descrypt()

--- vigenere.py
+++ vigenere.py
@@ -10,6 +10,31 @@ def encrypt_vigenere(plaintext, keyword):
     'LXFOPVEFRNHR'
     """
     # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    i = 0
+    while len(keyword) < len(plaintext):
+    	keyword += keyword[i]
+    	i += 1
+
+    ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabet = ALPHABET.lower()
+    ciphertext = ''
+
+    for i in range(len(plaintext)):
+
+    	ch = plaintext[i]
+    	k = keyword[i]
+
+    	if ch in alphabet:
+    		ciphertext += alphabet[(alphabet.index(ch)+ALPHABET.index(k))%26]
+
+    	elif ch in ALPHABET:
+    		ciphertext += ALPHABET[(ALPHABET.index(ch)+ALPHABET.index(k))%26]
+
+    	else:
+    		ciphertext += ch
+
+
     return ciphertext
 
 
@@ -24,5 +49,30 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    i = 0
+    while len(keyword) < len(ciphertext):
+    	keyword += keyword[i]
+    	i += 1
+
+    N_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ALPHABET = 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
+    alphabet = ALPHABET.lower()
+    plaintext = ''
+
+    for i in range(len(ciphertext)):
+
+    	ch = ciphertext[i]
+    	k = keyword[i]
+
+    	if ch in alphabet:
+    		plaintext += alphabet[(alphabet.index(ch)+N_ALPHABET.index(k))%26]
+
+    	elif ch in ALPHABET:
+    		plaintext += ALPHABET[(ALPHABET.index(ch)+N_ALPHABET.index(k))%26]
+
+    	else:
+    		plaintext += ch
+
+
     return plaintext
